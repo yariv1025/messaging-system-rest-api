f5ae4c9bbe0321b55b29efaed07c60d864a86606
Yariv Garala <yariv1052@gmail.com>
2021-10-28 17:44:56 +0300

    Adding auth decorator and login route

--- src/app.py
+++ src/app.py
@@ -1,14 +1,11 @@
 import ast
 import os
 
-from bson import ObjectId
 from flask import Flask
 from flask_pymongo import PyMongo
-
 from src import auth
 from src.seed.seeder import seed
 from src.users.models import *
-from src.messages.models import Message
 
 # app initialization
 app = Flask(__name__)
@@ -38,14 +35,39 @@ mongo_client = PyMongo(app)
 collection = mongo_client.db
 
 
+# TODO: Move authorize_user() to __init__.py in auth dir
+def authorize_user(func):
+    """
+    Extension of code on an existing function.
+    The authorize_user decorator performs authentication and then returns to the original function
+    :param func: Another function
+    :return: original func(user_id) with user id as arg
+    """
+
+    # Authorization
+    def wrap_auth_check(**kwargs):
+
+        access_token = request.headers['Authorization'].split()[1]
+        token = collection.tokens.find_one({"access_token": access_token})
+
+        if token:
+            print("token: ", token)
+            print("token[user_id]: ", token["user_id"])
+            return func(token["user_id"], kwargs)
+        else:
+            return tools.JsonResp("Unauthorized access", 401)
+
+    return wrap_auth_check
+
+
 #  Home page
-@app.route("/")
+@app.route('/')
 def home_page():
     return "Messaging System - RESTful API!"
 
 
 #  Home page
-@app.route("/seed", methods=['POST'])
+@app.route('/seed', methods=['POST'])
 def seed_db():
     seed(collection)
     return "SEED"
@@ -63,10 +85,7 @@ def get_all_messages():
         return User.read_all_messages(collection)
 
     except Exception as e:
-        return {
-            "status_code": 500,
-            "message": str(e)
-        }
+        return {"error": str(e)}, 500
 
 
 # Get all unread messages for specific user
@@ -77,26 +96,26 @@ def get_all_unread_messages(is_read):
 
 # Get only one message by id
 @app.route('/read-message/<string:messageId>', methods=['GET'])
-def read_message(messageId):
+@authorize_user
+def read_message(user_id, messageId):
     """
     Query message where the `id` field equal messageId
+    :param user_id: user id
     :param messageId: message identification number
     :return: Details of one message
     """
 
     # TODO:
-    # 1) Get user id -> DONE
+    # 1) Get user id -> X
     # 2) Query the user from db -> X
     # 2) Check authentication -> X
     # 3) get message by id -> X
+
     try:
-        return User.read_message(collection, messageId)
+        return User.read_message(collection, messageId["messageId"])
 
     except Exception as e:
-        return {
-            "status_code": 500,
-            "message": str(e)
-        }
+        return {"error": str(e)}, 500
 
 
 # Delete message (as owner or as receiver)
@@ -112,10 +131,7 @@ def delete_message(messageId):
         return User.delete_message(collection, messageId)
 
     except Exception as e:
-        return {
-            "status_code": 500,
-            "message": str(e)
-        }
+        return {"error": str(e)}, 500
 
 
 # Write message
@@ -129,10 +145,7 @@ def write_messages():
         return User.send_message(collection)
 
     except Exception as e:
-        return {
-            "status_code": 500,
-            "message": str(e)
-        }
+        return {"error": str(e)}, 500
 
 
 # User creation
@@ -144,39 +157,86 @@ def create_user():
                 request.args['email'].lower(),
                 request.args['password'])
 
-    # Make sure there isn"t already a user with this email address
-    existing_email = collection.users.find_one({"email": request.args['email'].lower()})
+    # Looking for user
+    user_response = collection.users.find_one({"email": request.args['email'].lower()})
 
-    if tools.validEmail(request.args['email']) and existing_email is None:
+    if tools.validEmail(request.args['email']) and user_response is None:
 
         try:
             response = user.save(collection)
-            return Response(json.dumps(response.inserted_id, default=json_util.default), mimetype="application/json")
+            return tools.JsonResp(response.inserted_id, 200)
 
         except Exception as e:
-            return {"error": str(e)
-                    }, 500
+            return {"error": str(e)}, 500
 
     else:
-        return {
-                   "message": "There's already an account with this email address",
-                   "error": "email_exists"
-               }, 409
+        return {"message": "There's already an account with this email address",
+                "error": "email_exists"
+                }, 409
 
 
-@app.route('/user', methods=['POST'])
+@app.route('/sign-in', methods=['POST'])
 def login():
-    # response_user = user.save(collection)
-    # user_id = ast.literal_eval(json.dumps(response_user.inserted_id, default=json_util.default))["$oid"]
+    try:
+        # Get param & create the User object
+        sign_in_details = {"email": request.args['email'].lower(),
+                           "password": request.args['password']}
+
+        # Looking for user
+        user_response = collection.users.find_one({"email": request.args['email'].lower()})
+
+        if pbkdf2_sha256.verify(sign_in_details["password"], user_response["password"]) and user_response:
+            """
+            Check:
+            - that the password in the database is not the old hash
+            - that the password in the database is a correct hash of the password
+            - that the user is exist
+            """
+
+            user_id = json.dumps(user_response['_id'], default=json_util.default)
+
+            access_token = auth.encodeAccessToken(user_id, user_response["email"])
+            refresh_token = auth.encodeRefreshToken(user_id, user_response["email"])
+
+            # TODO: Fix & replace with update method from User class
+            collection.tokens.update_many({"user_id": ObjectId(user_response['_id'])},
+                                          {"$set": {"access_token": access_token,
+                                                    "refresh_token": refresh_token,
+                                                    "last_login": tools.nowDatetimeUTC()
+                                                    }}, upsert=True)
+
+            resp = tools.JsonResp({
+                "id": user_response["_id"],
+                "email": user_response["email"],
+                "first_name": user_response["first_name"],
+                "last_name": user_response["last_name"],
+                "access_token": access_token,
+                "refresh_token": refresh_token
+            }, 200)
+
+            return resp
+
+        else:
+            return tools.JsonResp({"message": "Invalid user credentials"}, 403)
+
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+
+@app.route('/sign-out', methods=['GET'])
+def logout():
+    # try:
+    # TODO: Temporary SECRET_KEY!! -> config.cfg file needed fixing
+    # SECRET_KEY = 'aQ70AYYoi4'
+    # token_data = jwt.decode(request.headers.get("AccessToken"), SECRET_KEY)
+    # print("token_data: ", token_data)
+    # collection.users.update({"id": token_data["user_id"]}, {'$unset': {"refresh_token": ""}})
     #
-    # # Log the user in (create and return tokens)
-    # access_token = auth.encodeAccessToken(user_id, user.get_user()["email"])
-    # refresh_token = auth.encodeRefreshToken(user_id, user.get_user()["email"])
-    #
-    # data_to_update = {"refresh_token": refresh_token, "access_token": access_token}
-    #
-    # response = user.update_user(collection, response_user.inserted_id, data_to_update)
-    # print("CHECK11")
+    # Implement logout & token revoking/blacklisting needed
+    # Info : https://flask-jwt-extended.readthedocs.io/en/stable/blocklist_and_token_revoking/
+
+    # except Exception as e:
+    #     return {"error": str(e)}, 500
     pass
 
 
@@ -193,5 +253,7 @@ def create_update_list(internal_update=None):
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
 
-# TODO: Handling error returns
-# TODO: Grab user/pass from the environment variables (config.cfg file?)
+# TODO: Check to handle error returns
+# TODO: Grab user/pass from the environment variables (config.cfg file)
+# TODO: Fix methods -> from static to instance
+# TODO: Change method name convention to camelCase
